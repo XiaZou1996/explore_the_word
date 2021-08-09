f18400b043c84fc787ff5782ee40cd5b5d51a036
XiaZou1996 <81738751+XiaZou1996@users.noreply.github.com>
2021-08-09 15:55:24 +0800

    commit

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -4,8 +4,8 @@ class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-        
-        self.image = pygame.image.load('images/ship.bmp')
+        """IDE USE r to read the ship.bmp from a absolute path"""
+        self.image = pygame.image.load(r'D:\code\explore_the_word\alien_invasion\images\ship.bmp') 
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
